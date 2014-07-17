2a47af328dc2a1e52bf57614edbce2ec3b1782bf
Chris Rodgers <xrodgers@gmail.com>
2014-07-16 21:12:30 -0400

    basic loading functions

---
+++ __init__.py
@@ -0,0 +1,21 @@
+"""Module for dealing with whiski data.
+
+
+This directory, or one like it, should be on sys.path:
+/home/chris/Downloads/whisk-1.1.0d-Linux/share/whisk/python
+
+It's not ideal because that directory contains a lot of files with common
+names (and no __init__.py), so probably put it on the end of the path.
+"""
+
+# Import whiski files from where they live.
+# This will trigger a load of default.parameters, always in the directory
+# in which the calling script lives, NOT the module directory.
+# How to fix that??
+import traj
+import trace
+
+
+# Import the functions for analyzing data
+from base import *
+
