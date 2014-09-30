53aba38bb2029d7b9d9b562f3e212d7a4bd8af62
Chris Rodgers <xrodgers@gmail.com>
2014-09-29 22:14:57 -0400

    rewrote this placeholder function

--- overlay_whiskers_onto_video.py
+++ overlay_whiskers_onto_video.py
@@ -1,134 +1,117 @@
-# Dump frames from video, with and without the tracked whisker outline
-# Whisk imports
-import sys
-sys.path.insert(1, 
-    '/home/chris/Downloads/whisk-1.1.0d-Linux/share/whisk/python')
-import traj
-import trace
-
-# Other imports
 import matplotlib.pyplot as plt
 import numpy as np, os.path, pandas, my, scipy.misc
 
-# Data location
-#~ session = '0509A_cropped_truncated_4'; side = 'top'
-session = '0527_cropped_truncated_5'; side = 'left'
-whisk_rootdir = os.path.expanduser('~/mnt/bruno-nix/whisker_video/processed')
-whisk_file = os.path.join(whisk_rootdir, session, session + '.whiskers')
-measure_file = os.path.join(whisk_rootdir, session, session + '.measurements')
-video_file = os.path.join(whisk_rootdir, session, session + '.mp4')
+def make_movie_tracked_whiskers_and_angle(output_dir, 
+    video_file, video_frame_times,
+    angles_to_plot, angles_to_plot_t, 
+    downsample_ratio=10, truncate=None,
+    piv_ws=None, video_frame_numbers=None,
+    temp_output_file='temp.png', dpi=200, ylims=None):
+    """Writes out frames with tracked whiskers and calculated angles
+    
+    Creates a figure with two subplots. The top will be an image from
+    the video with the whiskers overlaid. The bottom will be a scrolling
+    graph of the tracked angle versus time.
+    
+        output_dir : where to put frames
+        video_file : source video to extract frames from
+        video_frame_times : time in video timebase to extract each frame
+            This will be downsampled by `downsample_ratio`, and that sets
+            the output framerate.
+        angles_to_plot : 2d array. Each column will be plotted vs time.
+            Generally this is some average angle, or individual whisker angles.
+            This must be the same size as video_frame_times, so that it knows
+            how to link up the trace with the video!
+        angles_to_plot_t : time values against which to plot the angle.
+            This serves no purpose other than aesthetics.
+            Must be same length as angles_to_plot.
+        piv_ws : DataFrame of whisker x and y to overlay.
+            Must be indexed by frame number.
+        video_frame_numbers : how to index into angles_to_plot.
+            Must be same length as video_frame_times.
+            For each frame dumped, the corresponding frame number here is
+            used to index into piv_ws and extract the corresponding whisker
+            traces.
+    
+    Note: I previously commented this:  
+        # FFMPEG needs to know the time of the frame
+        # It seems to round up, strangely, so subtract a ms and then round to the ms
+        frametime = np.round((frame / video_framerate) - .001, 3)
+    
+    Afterwards, use something like this to create the video:
+    ffmpeg -r 30 -i %06d.png -y -vcodec mpeg4 out.mp4
+    """
+    assert len(angles_to_plot) == len(video_frame_times)
 
-# Also load angles
-angl_df = pandas.load('angles_%s' % session)
-piv_angle = angl_df.pivot_table(rows='frame', cols='wid', 
-    values=['angle', 'angle2'])
-metric = 'angle2'
+    if ylims is None:
+        ylims = (angles_to_plot.min() - 20, angles_to_plot.max() + 20)
+    
+    if not os.path.exists(output_dir):
+        print "creating", output_dir
+        os.mkdir(output_dir)
 
-# Where to put the resulting video
-output_dir = os.path.join('/home/chris/mnt/bruno-nix/side_by_side', session)
-if not os.path.exists(output_dir):
-    os.mkdir(output_dir)
-
-# Load the traces
-frame2segment_id2whisker_seg = trace.Load_Whiskers(whisk_file)
-
-# Load the correspondence between traces and identified whiskers
-tmt = traj.MeasurementsTable(measure_file)
-whisker_id2frame2segment_id = tmt.get_trajectories()
-
-# Identify available frames
-frames_l = frame2segment_id2whisker_seg.keys()
-sorted_frames = np.sort(frames_l)
-
-# Set the color cycle
-if len(whisker_id2frame2segment_id) == 3:
-    whiski_color_cycle = ['r', 'g', 'b']
-else:
-    whiski_color_cycle = ['r', 'y', 'g', 'cyan', 'b', 'magenta']
-
-# Truncate
-#~ sorted_frames = sorted_frames[sorted_frames >= 4950]
-
-# Whether to show traces
-SHOW_TRACES = False
-STACKING = 'vertical'
-
-# Create figure
-if SHOW_TRACES:
+    ## Initialize figure
     f, axa = plt.subplots(2, 1, figsize=(5, 8.2))
     f.subplots_adjust(left=.15, right=.95, top=.97, bottom=.05)
-    ax = axa[0]
 
     # Also plot the angle
-    axa[1].set_color_cycle(whiski_color_cycle)
     axa[1].set_xlabel('time (s)')
     axa[1].set_ylabel('whisker angle (degrees)')
 
     # Full time course
-    frame_nums = piv_angle[metric].index.values
-    axa[1].plot(frame_nums / 300., piv_angle[metric].ix[frame_nums])
-    axa[1].set_ylim((-140, -80))
+    axa[1].plot(angles_to_plot_t, angles_to_plot)
+    axa[1].set_ylim(ylims)
 
     # This will be used to show the current time
-    time_line, = axa[1].plot([0, 0], [-140, -80], 'k')
-else:
-    f, ax = plt.subplots(figsize=(5, 6))
+    time_line, = axa[1].plot([0, 0], axa[1].get_ylim(), 'k')
 
 
-# Iterate over frames
-frames_to_check = sorted_frames[
-    (sorted_frames >= 300) &
-    (sorted_frames < 900)]
-for nframe, frame in enumerate(frames_to_check):
-    # FFMPEG needs to know the time of the frame
-    # It seems to round up, strangely, so subtract a ms and then round to the ms
-    frametime = np.round((frame / 30.0) - .001, 3)
-    
-    # status
-    print nframe, frame, frametime
-    
-    # Dump the frame
-    my.misc.frame_dump(video_file, frametime=frametime, output_filename='out.png')
-    
-    # Load it
-    im = scipy.misc.imread('out.png', flatten=True)
-    
-    # Create a copy with whisker trace overlaid
-    if STACKING == 'horizontal':
-        concatted = np.concatenate([im, im], axis=1)
-    if STACKING == 'vertical':
-        concatted = np.concatenate([im, im], axis=0)
-    
-    # Display concatted
-    ax.imshow(concatted, interpolation='nearest', cmap=plt.cm.gray)
-    for imobj in ax.get_images():
-        imobj.set_clim((0, 255))
+    ## Iterate over frames
+    n_output_frame = 0
+    for n_input_frame in range(0, len(video_frame_times), downsample_ratio):
+        if truncate is not None and n_output_frame > truncate:
+            break
+        
+        # Get the time of the frame to dump
+        frametime = video_frame_times[n_input_frame]
 
-    # Also load and plot the whiskers from this frame
-    for wid in whisker_id2frame2segment_id:
-        try:
-            segment_id = whisker_id2frame2segment_id[wid][frame]
-        except KeyError:
-            # can't find whisker in this frame
-            continue
-        ws = frame2segment_id2whisker_seg[frame][segment_id]
-        ax.plot(ws.x, ws.y, whiski_color_cycle[wid])
+        # status
+        print n_output_frame, n_input_frame, frametime
+        
+        # Dump the frame and re-load it
+        my.misc.frame_dump(video_file, frametime=frametime, 
+            output_filename=temp_output_file)
+        im = scipy.misc.imread(temp_output_file, flatten=True)
 
-    # Limits match axis
-    ax.set_xlim((0, concatted.shape[1] - 1))
-    ax.set_ylim((concatted.shape[0] - 1, 0))
-    
-    if SHOW_TRACES:
+        # Now plot it
+        axa[0].imshow(im, interpolation='nearest', cmap=plt.cm.gray)
+        for imobj in axa[0].get_images():
+            imobj.set_clim((0, 255))
+
+        # Also load and plot the whiskers from this frame
+        if piv_ws is not None and video_frame_numbers is not None:
+            # Index into original frames
+            original_frame_number = video_frame_numbers[n_input_frame]
+            frame_whiskers = piv_ws.ix[original_frame_number]
+            
+            for wid in frame_whiskers.index:
+                wsx = frame_whiskers['wsx'][wid]
+                wsy = frame_whiskers['wsy'][wid]
+                axa[0].plot(wsx, wsy)#whiski_color_cycle[wid])
+
+        # Limits match axis
+        axa[0].set_xlim((0, im.shape[1] - 1))
+        axa[0].set_ylim((im.shape[0] - 1, 0))
+        
         # Update the traces
-        axa[1].set_xlim((frame / 300. - 1.0, frame / 300. + 1.0))
-        time_line.set_xdata(np.array([frame, frame]) / 300.)
-    
-    # Save and prepare to ffmpeg
-    f.savefig(os.path.join(output_dir, '%06d.png' % nframe), dpi=200)
-
-    # Clear the image
-    ax.clear()
-
-# And then run something like this on the server
-# ffmpeg -r 30 -i %06d.png -y -vcodec mpeg4 out.avi
-    
+        # Index by n_input_frame to get the corresponding times in whisker angle
+        whisker_time = angles_to_plot_t[n_input_frame]
+        axa[1].set_xlim((whisker_time - 1.0, whisker_time + 1.0))
+        time_line.set_xdata(np.array([whisker_time, whisker_time]))
+        
+        # Save and prepare to ffmpeg
+        f.savefig(os.path.join(output_dir, '%06d.png' % n_output_frame), dpi=dpi)
+        n_output_frame = n_output_frame + 1
+        
+        # Clear the image
+        axa[0].clear()    
