111e04279726cdeea9406d2b57d2f1b3162caaec
Chris Rodgers <xrodgers@gmail.com>
2014-09-29 20:17:52 -0400

    moving angle calculations into functions

--- base.py
+++ base.py
@@ -53,48 +53,9 @@ def load_whisker_positions(whisk_file, measure_file, side='left'):
             # Get the actual segment for this whisker and frame
             ws = frame2segment_id2whisker_seg[frame][segment_id]
             
-            # fit a line and calculate angle of whisker
-            # in the video, (0, 0) is upper left, so we need to take negative of slope
-            # This will fail for slopes close to vertical, for instance if it
-            # has this shape: (  because least-squares fails here
-            # eg Frame 5328 in 0509A_cropped_truncated_4
-            p = np.polyfit(ws.x, ws.y, deg=1)
-            slope = -p[0]
-
-            # Arctan gives values between -90 and 90
-            # Basically, we cannot discriminate a SSW whisker from a NNE whisker
-            # Can't simply use diff_x because the endpoints can be noisy
-            # Similar problem occurs with ESE and WNW, and then diff_y is noisy
-            # Easiest way to do it is just pin the data to a known range
-            angle = np.arctan(slope) * 180 / np.pi
-            
-            # side = left, so theta ~-90 to +90
-            # side = top, so theta ~ -180 to 0
-            if side == 'top':
-                if angle > 0:
-                    angle = angle - 180
-            elif side == 'left':
-                if angle > 90:
-                    angle = angle - 180
-
-            # Separate angle measurement: tip vs follicle
-            # This will be noisier
-            # Remember to flip up/down here
-            # Also remember that ws.x and ws.y go from tip to follicle (I think?)
-            # Actually the go from tip to follicle in one video and from follicle
-            # to tip in the other; and then occasional exceptions on individual frames
-            angle2 = np.arctan2(
-                -(ws.y[0] - ws.y[-1]), ws.x[0] - ws.x[-1]) * 180 / np.pi
-
-            # On rare occasions it seems to be flipped, 
-            # eg Frame 9 in 0509A_cropped_truncated_4
-            # So apply the same fix, even though it shouldn't be necessary here
-            if side == 'top':
-                if angle2 > 0:
-                    angle2 = angle2 - 180
-            elif side == 'left':
-                if angle2 > 90:
-                    angle2 = angle2 - 180
+            # Fit angle two ways
+            angle = angle_meth1(ws.x, ws.y, side)
+            angle2 = angle_meth2(ws.x, ws.y, side)
 
             # Store
             rec_l.append({
@@ -106,3 +67,62 @@ def load_whisker_positions(whisk_file, measure_file, side='left'):
         #~ values=['angle', 'angle2'])
     
     return angl_df
+
+
+def angle_meth1(wsx, wsy, side):
+    """Fit angle by lstsqs line fit, then arctan, then pin.
+    
+    This will fail for slopes close to vertical.
+    """
+    # fit a line and calculate angle of whisker
+    # in the video, (0, 0) is upper left, so we need to take negative of slope
+    # This will fail for slopes close to vertical, for instance if it
+    # has this shape: (  because least-squares fails here
+    # eg Frame 5328 in 0509A_cropped_truncated_4
+    p = np.polyfit(wsx, wsy, deg=1)
+    slope = -p[0]
+
+    # Arctan gives values between -90 and 90
+    # Basically, we cannot discriminate a SSW whisker from a NNE whisker
+    # Can't simply use diff_x because the endpoints can be noisy
+    # Similar problem occurs with ESE and WNW, and then diff_y is noisy
+    # Easiest way to do it is just pin the data to a known range
+    angle = np.arctan(slope) * 180 / np.pi    
+    
+    # pin
+    pinned_angle = pin_angle(angle, side)
+    
+    return pinned_angle
+
+def angle_meth2(wsx, wsy, side):
+    """Fit angle by arctan of tip vs follicle, then pin"""
+    # Separate angle measurement: tip vs follicle
+    # This will be noisier
+    # Remember to flip up/down here
+    # Also remember that ws.x and ws.y go from tip to follicle (I think?)
+    # Actually the go from tip to follicle in one video and from follicle
+    # to tip in the other; and then occasional exceptions on individual frames
+    angle = np.arctan2(
+        -(wsy[0] - wsy[-1]), wsx[0] - wsx[-1]) * 180 / np.pi
+
+    # On rare occasions it seems to be flipped, 
+    # eg Frame 9 in 0509A_cropped_truncated_4
+    # So apply the same fix, even though it shouldn't be necessary here
+    # pin
+    pinned_angle = pin_angle(angle, side)
+    
+    return pinned_angle    
+
+def pin_angle(angle, side):
+    """Pins angle to normal range, based on side"""
+    # side = left, so theta ~-90 to +90
+    # side = top, so theta ~ -180 to 0    
+    
+    if side == 'top':
+        if angle > 0:
+            return angle - 180
+    elif side == 'left':
+        if angle > 90:
+            return angle - 180
+    return angle
+    
