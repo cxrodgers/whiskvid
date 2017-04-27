7c92428503e8db5723f9fe9ad3f85a2ffb06d238
Chris Rodgers <xrodgers@gmail.com>
2017-04-27 17:58:05 -0400

    removing deprecated loading functions that were inconsistent anyway

--- base.py
+++ base.py
@@ -17,7 +17,7 @@
 * Plotting stuff, basically part of the pipeline
 """
 try:
-    import traj, trace
+    from whisk.python import traj, trace
 except ImportError:
     pass
 import numpy as np, pandas
@@ -42,82 +42,6 @@ WHISKER_COLOR_ORDER_W = [
     'white', 'b', 'g', 'r', 'c', 'm', 'y', 'pink', 'orange']
 WHISKER_COLOR_ORDER_K = [
     'k', 'b', 'g', 'r', 'c', 'm', 'y', 'pink', 'orange']
-
-def load_whisker_traces(whisk_file):
-    """Load the traces, return as frame2segment_id2whisker_seg"""
-    frame2segment_id2whisker_seg = trace.Load_Whiskers(whisk_file)
-    return frame2segment_id2whisker_seg
-
-def load_whisker_identities(measure_file):
-    """Load the correspondence between traces and identified whiskers
-    
-    Return as whisker_id2frame2segment_id
-    """
-    tmt = traj.MeasurementsTable(measure_file)
-    whisker_id2frame2segment_id = tmt.get_trajectories()
-    return whisker_id2frame2segment_id
-
-def load_whisker_positions(whisk_file, measure_file, side='left'):
-    """Load whisker data and return angle at every frame.
-    
-    This algorithm needs some work. Not sure the best way to convert to
-    an angle. See comments.
-    
-    Whisker ids, compared with color in whiski GUI:
-    (This may differ with the total number of whiskers??)
-        -1  orange, one of the unidentified traces
-        0   red
-        1   yellow
-        2   green
-        3   cyan
-        4   blue
-        5   magenta
-    
-    Uses `side` to disambiguate some edge cases.
-    
-    Returns DataFrame `angl_df` with columns:
-        frame: frame #
-        wid: whisker #
-        angle: angle calculated by fitting a polynomial to trace
-        angle2: angle calculated by slope between endpoints.
-
-    `angle2` is noisier overall but may be more robust to edge cases.
-    
-    You may wish to pivot:
-    piv_angle = angl_df.pivot_table(rows='frame', cols='wid', 
-        values=['angle', 'angle2'])    
-    """
-    
-    
-    # Load whisker traces and identities
-    frame2segment_id2whisker_seg = load_whisker_traces(whisk_file)
-    whisker_id2frame2segment_id = load_whisker_identities(measure_file)
-    
-    # It looks like it numbers them from Bottom to Top for side == 'left'
-    # whiski colors them R, G, B
-
-    # Iterate over whiskers
-    rec_l = []
-    for wid, frame2segment_id in whisker_id2frame2segment_id.items():
-        # Iterate over frames
-        for frame, segment_id in frame2segment_id.items():
-            # Get the actual segment for this whisker and frame
-            ws = frame2segment_id2whisker_seg[frame][segment_id]
-            
-            # Fit angle two ways
-            angle = angle_meth1(ws.x, ws.y, side)
-            angle2 = angle_meth2(ws.x, ws.y, side)
-
-            # Store
-            rec_l.append({
-                'frame': frame, 'wid': wid, 'angle': angle, 'angle2': angle2})
-
-    # DataFrame it
-    angl_df = pandas.DataFrame.from_records(rec_l)
-    #~ piv_angle = angl_df.pivot_table(rows='frame', cols='wid', 
-        #~ values=['angle', 'angle2'])
-    
-    return angl_df
 
 def angle_meth1(wsx, wsy, side):
     """Fit angle by lstsqs line fit, then arctan, then pin.
