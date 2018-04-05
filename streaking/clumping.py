113090576a9b5129fbdc02fe299739c99794a551
Chris Rodgers <xrodgers@gmail.com>
2018-04-04 23:07:29 -0400

    control over method of clumping; removing old code

--- streaking/clumping.py
+++ streaking/clumping.py
@@ -3,7 +3,7 @@ import numpy as np
 from base import calculate_center2center_distance_on_merged
 import scipy.optimize
 
-def clump_segments_into_streaks(mwe, threshold=32.0):
+def clump_segments_into_streaks(mwe, threshold=32.0, method='greedy'):
     ## Clump segments into streaks
     mwe = mwe.copy()
     
@@ -25,8 +25,7 @@ def clump_segments_into_streaks(mwe, threshold=32.0):
     # Can do this greedily or hungarian
     # It ends up being basically the same but hungarian takes longer
     # (mostly due to pandas operations)
-    method = 'first' # or 'hungarian'
-    if method == 'first':
+    if method == 'greedy':
         # Get sorted costs
         sorted_costs = pandas.concat([merged, dist], axis=1).loc[:, 
             ['frame0', 'index0', 'index1', 'cost']].sort_values('cost')
@@ -94,133 +93,62 @@ def clump_segments_into_streaks(mwe, threshold=32.0):
     
     ## Propagate assignments
     # This is now the heaviest part of the algorithm
-    # method 1: more legible
-    method = 'faster'
-    if method == 'legible':
-        mwe['streak'] = range(len(mwe))
-        # Propagate
-        for frame, subass in assignments.groupby(level=0):
-            mwe.loc[subass['index1'].values, 'streak'] = (
-                mwe.loc[subass.index.get_level_values(1), 'streak'].values)
-
-        # int
-        mwe['streak'] = mwe['streak'].rank(method='dense').astype(np.int)
+    # Get streaks, frames, and indexes as arrays
+    streakass = np.arange(len(mwe)).astype(np.int)
+    frameidx = assignments.index.get_level_values(level='frame0').values
+    idx0 = assignments.index.get_level_values(level='index0')
+    idx1 = assignments['index1'].values
     
-    # method 2: faster
-    elif method == 'fast':
-        # Get streaks, frames, and indexes as arrays
-        streakass = np.arange(len(mwe)).astype(np.int)
-        frameidx = assignments.index.get_level_values(level='frame0').values
-        idx0 = assignments.index.get_level_values(level='index0')
-        idx1 = assignments['index1'].values
-        
-        # ilocify the indexes into streakass
-        ser = pandas.Series(range(len(mwe)), index=mwe.index)
-        iidx0 = ser.loc[idx0].values
-        iidx1 = ser.loc[idx1].values
-        
-        # propagate
-        for frame in assignments.index.levels[0]:
-            frame_mask = frameidx == frame
-            streakass[iidx1[frame_mask]] = streakass[iidx0[frame_mask]]
-        
-        # densify streakass
-        mwe['streak'] = streakass
-        mwe['streak'] = mwe['streak'].rank(method='dense').astype(np.int)
+    # ilocify the indexes into streakass
+    ser = pandas.Series(range(len(mwe)), index=mwe.index)
+    iidx0 = ser.loc[idx0].values
+    iidx1 = ser.loc[idx1].values
 
-    elif method == 'faster':
-        # Get streaks, frames, and indexes as arrays
-        streakass = np.arange(len(mwe)).astype(np.int)
-        frameidx = assignments.index.get_level_values(level='frame0').values
-        idx0 = assignments.index.get_level_values(level='index0')
-        idx1 = assignments['index1'].values
+    # This propagation series defines which ii (on index) is linked to
+    # which following ii (on values)
+    prop_ser = pandas.Series(iidx1, index=iidx0)
+
+    # Include only the nodes that start a chain
+    starts_only = prop_ser.drop(prop_ser.values, errors='ignore')
+
+    # Continually map the propagation series onto itself until we've
+    # reached the end of all chains
+    # Begin with the start of each chain
+    rec_l = [
+        pandas.Series(starts_only.index.values, index=starts_only.index)]
+    
+    # Propagate
+    res = starts_only.copy()
+    while True:
+        rec_l.append(res)
         
-        # ilocify the indexes into streakass
-        ser = pandas.Series(range(len(mwe)), index=mwe.index)
-        iidx0 = ser.loc[idx0].values
-        iidx1 = ser.loc[idx1].values
-
-        # This propagation series defines which ii (on index) is linked to
-        # which following ii (on values)
-        prop_ser = pandas.Series(iidx1, index=iidx0)
-
-        # Include only the nodes that start a chain
-        starts_only = prop_ser.drop(prop_ser.values, errors='ignore')
-
-        # Continually map the propagation series onto itself until we've
-        # reached the end of all chains
-        # Begin with the start of each chain
-        rec_l = [
-            pandas.Series(starts_only.index.values, index=starts_only.index)]
+        # dropna and intify are important here for speed
+        res = res.map(prop_ser).dropna().astype(np.int)
         
-        # Propagate
-        res = starts_only.copy()
-        while True:
-            rec_l.append(res)
-            
-            # dropna and intify are important here for speed
-            res = res.map(prop_ser).dropna().astype(np.int)
-            
-            # break if all chains are done
-            if len(res) == 0:
-                break
+        # break if all chains are done
+        if len(res) == 0:
+            break
 
-        # Concatenate all chains    
-        chains = pandas.concat(rec_l, axis=0, keys=range(len(rec_l)), 
-            verify_integrity=True).swaplevel().sort_index()
-        chains.index.names = ['start', 'link']
+    # Concatenate all chains    
+    chains = pandas.concat(rec_l, axis=0, keys=range(len(rec_l)), 
+        verify_integrity=True).swaplevel().sort_index()
+    chains.index.names = ['start', 'link']
 
-        # Error check no duplicates
-        assert not chains.duplicated().any()
+    # Error check no duplicates
+    assert not chains.duplicated().any()
 
-        # Error check that the length of all chains plus the number of
-        # singletons singletons equals the data length
-        n_singletons = (~np.in1d(streakass, chains.values)).sum()
-        assert n_singletons + len(chains) == len(streakass)
+    # Error check that the length of all chains plus the number of
+    # singletons singletons equals the data length
+    n_singletons = (~np.in1d(streakass, chains.values)).sum()
+    assert n_singletons + len(chains) == len(streakass)
 
-        # Assign
-        for start, this_chain in chains.groupby(level=0):
-            streakass[this_chain.values] = start
+    # Assign
+    for start, this_chain in chains.groupby(level=0):
+        streakass[this_chain.values] = start
 
-        # densify streakass
-        mwe['streak'] = streakass
-        mwe['streak'] = mwe['streak'].rank(method='dense').astype(np.int)
+    # densify streakass
+    mwe['streak'] = streakass
+    mwe['streak'] = mwe['streak'].rank(method='dense').astype(np.int)
 
-    # method 3: this doesn't converge
-    elif method == 'even_odd':
-        # Get streaks, frames, and indexes as arrays
-        streakass = np.arange(len(mwe)).astype(np.int)
-        frameidx = assignments.index.get_level_values(level='frame0').values
-        idx0 = assignments.index.get_level_values(level='index0')
-        idx1 = assignments['index1'].values
-        
-        # ilocify the indexes into streakass
-        ser = pandas.Series(range(len(mwe)), index=mwe.index)
-        iidx0 = ser.loc[idx0].values
-        iidx1 = ser.loc[idx1].values
-        
-        # propagate
-        even_frames = assignments.index.levels[0][::2].values
-        odd_frames = assignments.index.levels[0][1::2].values
-        
-        while True:
-            # do even
-            frame_mask = np.in1d(frameidx, even_frames)
-            n_to_change_even = (streakass[iidx1[frame_mask]] != streakass[iidx0[frame_mask]]).sum()
-            streakass[iidx1[frame_mask]] = streakass[iidx0[frame_mask]]
-            
-            # do odd
-            frame_mask = np.in1d(frameidx, odd_frames)
-            n_to_change_odd = (streakass[iidx1[frame_mask]] != streakass[iidx0[frame_mask]]).sum()
-            streakass[iidx1[frame_mask]] = streakass[iidx0[frame_mask]]            
-        
-            if n_to_change_even + n_to_change_odd == 0:
-                break
-            else:
-                print n_to_change_even, n_to_change_odd
-        
-        # densify streakass
-        mwe['streak'] = streakass
-        mwe['streak'] = mwe['streak'].rank(method='dense').astype(np.int)
 
     return mwe
